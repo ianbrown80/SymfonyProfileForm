201d259d5356a9e767bec9489336788ecaeeb89a
Ian <brown.i18@sky.com>
2017-02-21 03:07:49 +0000

    Update user page complete

--- src/AppBundle/Controller/HobbyController.php
+++ src/AppBundle/Controller/HobbyController.php
@@ -82,7 +82,44 @@ class HobbyController extends Controller
      */
     public function updateAction(Request $request, $id)
     {
-        return $this->render('AppBundle:Hobby:update.html.twig');
+      $user = $this->getDoctrine()
+        ->getRepository('AppBundle:User')
+        ->find($id);
+
+        $user->setName($user->getName());
+        $user->setBiography($user->getBiography());
+        $user->setImage($user->getImage());
+
+        $form = $this->createFormBuilder($user)
+          ->add('name', TextType::class, array('attr' => array('class' => 'form-control')))
+          ->add('biography', CKEditorType::class, array('attr' => array('class' => 'form-control')))
+          ->add('image', TextType::class, array('attr' => array('class' => 'form-control')))
+          ->add('save', SubmitType::class, array('label' => 'Update user', 'attr' => array('class' => 'btn btn-primary')))
+          ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          $name = $form['name']->getData();
+          $biography = $form['biography']->getData();
+          $image = $form['image']->getData();
+
+          $em = $this->getDoctrine()->getManager();
+          $user = $em->getRepository('AppBundle:User')
+            ->find($id);
+
+          $user->setName($name);
+          $user->setBiography($biography);
+          $user->setImage($image);
+
+          $em->flush();
+
+          $this->addFlash('notice', 'User updated');
+
+          return $this->redirectToRoute('homepage');
+        }
+        return $this->render('AppBundle:Hobby:update.html.twig',
+          array('form' => $form->createView())
+        );
     }
 
     /**
